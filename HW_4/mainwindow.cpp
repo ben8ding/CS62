83206fa2aa137bced0033962c4b49afe90aefdcb
Benjamin Ding <ben8ding@gmail.com>
2026-02-19 23:29:48 -0800

    Finish HW_4

--- HW_4/mainwindow.cpp
+++ HW_4/mainwindow.cpp
@@ -12,45 +12,63 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     //setup network
-    network_.readUsers("cycle5.txt");
-    //network_.readPosts("posts.txt");
+    network_.readUsers("users.txt");
+    network_.readPosts("posts.txt");
 
     loggedInUser_ = nullptr;
     currentUser_ = nullptr;
 
     ui->profileFriendList->setColumnCount(1);
+    ui->profileFriendSuggest->setColumnCount(1);
     //dynamic resize friendlist to name length
     ui->profileFriendList->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
     ui->profileFriendSuggest->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
-
-    ui->profileFriendSuggest->setColumnCount(1);
-
-
     toggleProfile(0);
+    ui->profileTitle->setAlignment(Qt::AlignCenter);
+    ui->profilePosts->setAlignment(Qt::AlignTop);
+    ui->profilePosts->setWordWrap(true);
 
-    //connect(ui->debugButton, &QPushButton::clicked, this, &MainWindow::loginUser);
+    //todo: move these to their own toggle
+    ui->profileReturnButton->setVisible(false);
+    ui->profileFriendSuggest->setVisible(false);
+    ui->profileFriendSuggestLabel->setVisible(false);
+    ui->profileAddFriendButton->setVisible(false);
+
     connect(ui->loginButton,
         &QPushButton::clicked,
         this,
         &MainWindow::loginUser);
+    //visit friend list on table click
     connect(ui->profileFriendList,
-            &QTableWidget::cellClicked,
-            this,
-            &MainWindow::setUserFromTable);
+        &QTableWidget::cellClicked,
+        this,
+        &MainWindow::setUserFromTable);
+    connect(ui->profileFriendSuggest,
+        &QTableWidget::cellClicked,
+        this,
+        &MainWindow::setUserFromSuggestions);
+
+    //return to friends list button
+    connect(ui->profileReturnButton,
+        &QPushButton::clicked,
+        this,
+        &MainWindow::returnToProfile);
+    connect(ui->profileAddFriendButton,
+        &QPushButton::clicked,
+        this,
+        &MainWindow::addFriend);
+    //add post
+    connect(ui->profileAddPostButton,
+        &QPushButton::clicked,
+        this,
+        &MainWindow::addPost);
 }
 
 void MainWindow::loginUser(){
-
     std::string name = ui->loginBox->toPlainText().toStdString();
-
     if(network_.getId(name) != -1){
         loggedInUser_ = network_.getUser(network_.getId(name));
-
-        ui->debugLabel1->setText(QString::fromStdString(loggedInUser_->getName()));
-        ui->debugLabel2->setText(QString::fromStdString(std::to_string(loggedInUser_->getId())));
-
         currentUser_ = network_.getUser(network_.getId(name));
-
         toggleLogin(0);
         updateCurrentProfile();
         toggleProfile(1);
@@ -60,11 +78,23 @@ void MainWindow::loginUser(){
     }
 }
 
-//todo: set current user as the selected user and update the profile
 void MainWindow::setUserFromTable(int row, int column){
-    //QTableWidgetItem *tempFriend = ui->profileFriendList->item(row, column);
-    //tempFriend->text();
-    ui->debugLabel1->setText(ui->profileFriendList->item(row, column)->text());
+    QTableWidgetItem *tempFriend = ui->profileFriendList->item(row, column);
+    std::string name = tempFriend->text().toStdString();
+    currentUser_ = network_.getUser(network_.getId(name));
+    updateCurrentProfile();
+}
+
+void MainWindow::setUserFromSuggestions(int row, int column){
+    QTableWidgetItem *tempFriend = ui->profileFriendSuggest->item(row, column);
+    std::string name = tempFriend->text().toStdString();
+    currentUser_ = network_.getUser(network_.getId(name));
+    updateCurrentProfile();
+}
+
+void MainWindow::returnToProfile(){
+    currentUser_ = loggedInUser_;
+    updateCurrentProfile();
 }
 
 void MainWindow::toggleLogin(int tog){
@@ -77,8 +107,10 @@ void MainWindow::toggleProfile(int tog){
     ui->profileTitle->setVisible(tog);
     ui->profileFriendListLabel->setVisible(tog);
     ui->profileFriendList->setVisible(tog);
-    ui->profileFriendSuggest->setVisible(tog);
-    ui->profileFriendSuggestLabel->setVisible(tog);
+    ui->profilePostsLabel->setVisible(tog);
+    ui->profilePosts->setVisible(tog);
+    ui->profilePostEdit->setVisible(tog);
+    ui->profileAddPostButton->setVisible(tog);
 }
 
 void MainWindow::updateCurrentProfile(){
@@ -91,16 +123,61 @@ void MainWindow::updateCurrentProfile(){
         ui->profileFriendList->setItem(iterator, 0, tempFriend);
         iterator++;
     }
-    //friend suggestions updater
-    int score;
-    std::vector<int> friendSuggestions = network_.suggestFriends(currentUser_->getId(), score);
-    ui->profileFriendSuggest->setRowCount(friendSuggestions.size());
 
-    for(int i = 0; i < friendSuggestions.size(); i++){
-        QTableWidgetItem *tempFriend = new QTableWidgetItem(QString::fromStdString(network_.getUser(friendSuggestions[i])->getName()));
-        ui->profileFriendSuggest->setItem(i, 0, tempFriend);
+
+    //todo unspaghetti by dedicating functions for showing/hiding non main profile stuff
+    if(currentUser_ == loggedInUser_){
+        //friend suggestions updater
+        ui->profileTitle->setText("My Profile");
+        ui->profileFriendSuggest->setVisible(true);
+        ui->profileFriendSuggestLabel->setVisible(true);
+        int score;
+        std::vector<int> friendSuggestions = network_.suggestFriends(currentUser_->getId(), score);
+        ui->profileFriendSuggest->setRowCount(friendSuggestions.size());
+
+        for(int i = 0; i < friendSuggestions.size(); i++){
+            QTableWidgetItem *tempFriend = new QTableWidgetItem(QString::fromStdString(network_.getUser(friendSuggestions[i])->getName()));
+            ui->profileFriendSuggest->setItem(i, 0, tempFriend);
+        }
+        ui->profileReturnButton->setVisible(false);
+    }
+    else{
+        ui->profileFriendSuggest->setVisible(false);
+        ui->profileFriendSuggestLabel->setVisible(false);
+        ui->profileReturnButton->setVisible(true);
+        std::string title = currentUser_->getName() + "'s Profile";
+        ui->profileTitle->setText(QString::fromStdString(title));
+        ui->profileAddFriendButton->setVisible(true);
     }
 
+    //debug panel stuff
+    ui->debugLabel1->setText(QString::fromStdString(currentUser_->getName()));
+    ui->debugLabel2->setText(QString::fromStdString(std::to_string(currentUser_->getId())));
+
+    updatePosts();
+    ui->profilePostEdit->clear();
+}
+
+void MainWindow::updatePosts(){
+    ui->profilePosts->setText(QString::fromStdString(network_.getPostsString(currentUser_->getId(),5)));
+}
+
+void MainWindow::addFriend(){
+    network_.addConnection(loggedInUser_->getName(), currentUser_->getName());
+    updateCurrentProfile();
+    network_.writeUsers("users.txt");
+}
+
+void MainWindow::addPost(){
+    if(ui->profilePostEdit->toPlainText() == ""){
+
+    }
+    else {
+    QString postText = ui->profilePostEdit->toPlainText();
+    network_.addPost(new Post(currentUser_->getId(), loggedInUser_->getId(), postText.toStdString(), 0));
+    updateCurrentProfile();
+    network_.writePosts("posts.txt");
+    }
 }
 
 MainWindow::~MainWindow()
--- HW_4/mainwindow.h
+++ HW_4/mainwindow.h
@@ -21,6 +21,10 @@ public:
 
 private slots:
     void setUserFromTable(int row, int column);
+    void setUserFromSuggestions(int row, int column);
+    void returnToProfile();
+    void addFriend();
+    void addPost();
 
 private:
     Ui::MainWindow *ui;
@@ -31,14 +35,26 @@ private:
     //user focused on
     User* currentUser_;
 
-    //pre: 1 to show, 0 to hide
-    //post: login elements are toggled to either show with 1, or hide with 0 for input
+    //Pre: 1 to show, 0 to hide
+    //Post: Login elements are toggled to either show with 1, or hide with 0 for input
     void toggleLogin(int tog);
 
+    //Pre: 1 to show, 0 to hide
+    //Post: (Some) profile elements are toggled to either show with 1, or hide with 0 for input
     void toggleProfile(int tog);
 
+    //Pre: The function is called (usually by a login user signal
+    //Post: The current user and logged in user are set to the given name and the main profile page is
+    //Todo: change this to a slot
     void loginUser();
 
+    //Pre:
+    //Post:
+    void updatePosts();
+
+    //Pre:
+    //Post:
+    //Todo: Document what this does once it's less spaghetti
     void updateCurrentProfile();
 };
-#endif // MAINWINDOW_H
+#endif
